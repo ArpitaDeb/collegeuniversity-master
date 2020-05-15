ace90b4e055c2635bfa6571fc7c0d4643a57eaa6
Arpita Deb <Arpitadeb1990@gmail.com>
2020-05-15 14:25:00 -0400

    first commit

---
+++ wp-content/themes/collegetheme/archive-campus.php
@@ -0,0 +1,20 @@
+<?php get_header(); 
+pageBanner(array(
+  'title' => 'Our Campuses',
+  'subtitle' => 'all locations'
+));
+?>
+<div class="container container--narrow page-section">
+<div class="acf-map">   
+  <?php  
+  while(have_posts()){
+  the_post();
+  $mapLocation = get_field('map_location');
+  ?>
+  <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lng']; ?>"> 
+  <?php } 
+  echo paginate_links();
+  ?>
+</div>
+</div>
+<?php get_footer();?>
